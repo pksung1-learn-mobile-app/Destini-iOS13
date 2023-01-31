656eaa45aa35770f15ffdf01b1de8d50cc423c87
vidi <pksung1@gmail.com>
2023-01-31 09:05:09 +0900

    finish

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -10,14 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
     @IBOutlet weak var storyLabel: UILabel!
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    var storyBrain: StoryBrain = StoryBrain()
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        updateUi()
     }
+    
+    @IBAction func choice1Pressed(_ sender: UIButton) {
+        storyBrain.nextStory(userChoice: sender.titleLabel?.text)
+        updateUi()
+    }
+    
+    
+    @IBAction func choice2Pressed(_ sender: UIButton) {
+        storyBrain.nextStory(userChoice: sender.titleLabel?.text)
+        updateUi()
+    }
+    
+    
+    func updateUi() {
+        let story = storyBrain.currentStory()
+        storyLabel.text = story.title
+        choice1Button.setTitle(story.choice1, for: .normal)
+        choice2Button.setTitle(story.choice2, for: .normal)
+    }
+    
 
 
 }
--- Destini-iOS13/Model/StoryBrain.swift
+++ Destini-iOS13/Model/StoryBrain.swift
@@ -9,3 +9,25 @@
 import Foundation
 
 
+struct StoryBrain {
+    
+    var storyNumber = 0
+    let stories: Array<Story> = [
+        Story(title: "You see a fork in the road", choice1: "Take a left", choice2: "Take a right"),
+        Story(title: "You see a tigger", choice1: "Shout for help", choice2: "Play dead"),
+        Story(title: "You find a treasure cheat", choice1: "Open it", choice2: "Check for trap")
+    ]
+    
+    mutating func nextStory(userChoice: String?) -> Void {
+        if (userChoice == stories[0].choice1) {
+            storyNumber = 1
+        } else {
+            storyNumber = 2
+        }
+    }
+    
+    func currentStory() -> Story {
+        return stories[storyNumber]
+    }
+    
+}
